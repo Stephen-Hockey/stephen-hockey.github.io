b953bcf9e554729aa19fba861ace891b11e06db3
Stephen Hockey <stephen.hockey.nz@gmail.com>
2023-11-21 02:23:49 +1300

    Made homepage ready for first release

--- js/script.js
+++ js/script.js
@@ -1,20 +1,14 @@
-// Honestly ChatGPT helped out but I plan to understand how exactly fetch, and especially 'then' works.
-function addNewsItem() {
-    fetch("news_item.html")
-        .then((response) => response.text())
-        .then((html) => {
-            document.getElementById("news").innerHTML += html;
-        })
-        .catch((error) => console.error("Error:", error));
-}
-
 function toggleNavMenu() {
     var navMenu = document.getElementById("nav-menu");
     navMenu.classList.toggle("visible");
 }
 
-addNewsItem();
-addNewsItem();
-addNewsItem();
-addNewsItem();
-addNewsItem();
+// For future use, and I was just learning how to create elements with js based on a file
+// function addNewsItem() {
+//     fetch("news_item.html")
+//         .then((response) => response.text())
+//         .then((html) => {
+//             document.getElementById("news").innerHTML += html;
+//         })
+//         .catch((error) => console.error("Error:", error));
+// }
